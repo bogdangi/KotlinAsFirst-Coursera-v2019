23b4849d5c5c4ce55f4a59a119b14688ae3c7e3d
Bogdan Girman <bogdan.girman@idealo.de>
2020-12-21 19:27:09 +0100

    Lesson 2: remove debug print

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -56,7 +56,6 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  */
 fun daysInMonth(month: Int, year: Int): Int {
     val leapYear = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
-    println("$year $month $leapYear ${leapYear && month == 2}")
 
     return when {
         leapYear && month == 2 -> 29
